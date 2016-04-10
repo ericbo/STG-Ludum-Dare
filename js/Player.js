c4af2d9d66ac7ad9bbec2d9de4fad1912c50d9d5
Zillapod <simon.pierre.brazeau@gmail.com>
2016-04-09 20:47:38 -0700

    Made player be able to move around and jump

--- js/Player.js
+++ js/Player.js
@@ -2,7 +2,12 @@ function Player () {
   this.x = 0;
   this.y = 0;
   this.speed = 2;
-  var shit = "fuck";
+  this.isjumping = false;
+  this.isOnGround = false;
+  this.curVel = 0;
+  this.maxVel = 10;
+  this.velIncrement = 0.2;
+  this.jumpVel = 5;
 
   this.model = {
     width: 32,
@@ -11,7 +16,34 @@ function Player () {
   };
 
   this.Update = function () {
-
+    if (this.y + this.model.height >= canvasHeight) {
+      this.isOnGround = true;
+      this.y = canvasHeight - this.model.height;
+      this.curVel = 0;
+    } else {
+      this.curVel -= this.velIncrement;
+      if (this.curVel > this.maxVel)
+        this.curVel = this.maxVel;
+        
+      this.y -= this.curVel;
+    }
+    
+    if (KEYS.RIGHT in KeysDown)
+      if (this.x + this.model.width < canvasWidth)
+        this.x += this.speed;
+        
+    if (KEYS.LEFT in KeysDown)
+      if (this.x > 0)
+        this.x -= this.speed;
+        
+    if (KEYS.SPACE in KeysDown)
+      if (this.isOnGround) {
+        console.log("Jumping!");
+        this.curVel = this.jumpVel;
+        this.y -= 1;
+        this.isOnGround = false;
+      }
+        
   };
 
   this.Render = function () {
