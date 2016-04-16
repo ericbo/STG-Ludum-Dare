0c3be366f41de5fc792cd897a5ab53f9bcc6e629
ericbo <eeb1010@gmail.com>
2016-04-16 14:13:52 -0400

    Added basic bullet class

---
+++ js/projectiles.js
@@ -0,0 +1,13 @@
+function Projectiles = function() {
+  this.x;
+  this.y;
+  this.prevX = 0;
+  this.prevY = 0;
+  this.speed = 6;
+  this.velIncrement = 0.5;
+  this.direction = 0; //0 - right, 1 - left
+
+  this.Update = function () {
+
+  }
+}
