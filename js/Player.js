49ce56a5f3d00a8ad39aa10935100581cd31fa6a
ericbo <eeb1010@gmail.com>
2016-04-16 08:59:49 -0400

    Made basica character animations more readable and expandable.

--- js/Player.js
+++ js/Player.js
@@ -29,11 +29,6 @@ function Player () {
     this.sprite = tmpImage;
   }
 
-  this.spriteCords = {
-    walking: [[6,88],[62,88],[116,88],[172,88]],
-    standing: [8, 6]
-  };
-
   this.Update = function () {
     var keyPressed = false;
 
@@ -106,41 +101,138 @@ function Player () {
     //ctx.fillStyle = "#fff";
     //ctx.fillRect(this.x, this.y, this.model.width, this.model.height);
 
-    //Draw here
-    var frames = this.spriteCords.walking.length;
-    var currentFrame = 0;
-    var frameTime = 6 / frames;
-    var elapsedTime = new Date().getTime() - this.start;
-
-    currentFrame = Math.floor((elapsedTime / 40) / frames) % frames;
-    ctx.draw
+    //Display correct animiation sprite based on the heros position.
     if(this.direction)
       if(this.moving)
         if(this.isOnGround)
-          ctx.drawImage(this.sprite, this.spriteCords.walking[currentFrame][0], this.spriteCords.walking[currentFrame][1], this.model.width, this.model.height, this.x, this.y, this.model.width, this.model.height);
+          this.DrawSprite(this.spriteState.walkingRight)
         else
-          ctx.drawImage(this.sprite, 228, 88, this.model.width, this.model.height, this.x, this.y, this.model.width, this.model.height);
+          this.DrawSprite(this.spriteState.jumpRight)
       else
-        ctx.drawImage(this.sprite, 8, 2, this.model.width, this.model.height, this.x, this.y, this.model.width, this.model.height);
+        this.DrawSprite(this.spriteState.idleRight);
     else {
-      ctx.save()
-      ctx.translate(canvasWidth, 0);
-      ctx.scale(-1, 1);
       if(this.moving)
         if(this.isOnGround)
-          ctx.drawImage(this.sprite, this.spriteCords.walking[currentFrame][0], this.spriteCords.walking[currentFrame][1], this.model.width, this.model.height, this.x - this.model.width, this.y, this.model.width, this.model.height);
+          this.DrawSprite(this.spriteState.walkingLeft);
         else
-          ctx.drawImage(this.sprite, 228, 88, this.model.width, this.model.height, this.x - this.model.width, this.y, this.model.width, this.model.height);
+          this.DrawSprite(this.spriteState.jumpLeft);
       else
-        ctx.drawImage(this.sprite, 8, 2, this.model.width, this.model.height, this.x - this.model.width, this.y, this.model.width, this.model.height);
-      ctx.restore();
+        this.DrawSprite(this.spriteState.idleLeft);
     }
+
     //Draw debug window
     ctx.fillStyle = "#48ff00";
     ctx.fillText("Current pos  (" + this.x + ", " + this.y + ")", 10, 10);
     ctx.fillText("Previous pos (" + this.prevX + ", " + this.prevY + ")", 10, 20);
     ctx.fillText("isOnGround - " + this.isOnGround, 10, 30);
     ctx.fillText("curVel - " + this.curVel, 10, 40);
+  };
 
+  /*
+  * Draws an image to the canvas context.
+  *
+  * @param state - An integer representing they type of sprite to be drawn.
+  *                 0) Idle, right
+  *                 1) Idle, left
+  *                 2) walk, right
+  *                 3) Walk, left
+  *                 4) Jump, right
+  *                 6) Jump, left
+  *                 7) Double jump, right
+  *                 8) Double jump, left
+  *                 9) Dash, right
+  *                10) Dash, left
+  *
+  * @returns nothing
+  */
+  this.DrawSprite = function(state) {
+    var elapsedTime = new Date().getTime() - this.start;
+    var frames = 1;
+    var currentFrame = 0;
+    //var
+
+    if(offset == 0) {
+
+    }
+
+    //Idle right
+    if(state == this.spriteState.idleRight) {
+      frames = this.spriteCords.idle.length;
+      currentFrame = Math.floor((elapsedTime / 40) / frames) % frames;
+
+      ctx.drawImage(this.sprite, this.spriteCords.idle[currentFrame][0], this.spriteCords.idle[currentFrame][1], this.model.width, this.model.height, this.x, this.y, this.model.width, this.model.height);
+      return;
+    }
+
+    //Idle left
+    if(state == this.spriteState.idleLeft) {
+      frames = this.spriteCords.idle.length;
+      currentFrame = Math.floor((elapsedTime / 40) / frames) % frames;
+
+      ctx.save()
+      ctx.translate(canvasWidth, 0);
+      ctx.scale(-1, 1);
+      ctx.drawImage(this.sprite, this.spriteCords.idle[currentFrame][0], this.spriteCords.idle[currentFrame][1], this.model.width, this.model.height, this.x - this.model.width, this.y, this.model.width, this.model.height);
+      ctx.restore();
+      return;
+    }
+
+    //Walking right
+    if(state == this.spriteState.walkingRight) {
+      frames = this.spriteCords.walking.length;
+      currentFrame = Math.floor((elapsedTime / 40) / frames) % frames;
+
+      ctx.drawImage(this.sprite, this.spriteCords.walking[currentFrame][0], this.spriteCords.walking[currentFrame][1], this.model.width, this.model.height, this.x, this.y, this.model.width, this.model.height);
+      return;
+    }
+
+    //Walking left
+    if(state == this.spriteState.walkingLeft) {
+      frames = this.spriteCords.walking.length;
+      currentFrame = Math.floor((elapsedTime / 40) / frames) % frames;
+
+      ctx.save()
+      ctx.translate(canvasWidth, 0);
+      ctx.scale(-1, 1);
+      ctx.drawImage(this.sprite, this.spriteCords.walking[currentFrame][0], this.spriteCords.walking[currentFrame][1], this.model.width, this.model.height, this.x - this.model.width, this.y, this.model.width, this.model.height);
+      ctx.restore();
+      return;
+    }
+
+    //Jumping right
+    if(state == this.spriteState.jumpRight) {
+      frames = this.spriteCords.jumping.length;
+      currentFrame = Math.floor((elapsedTime / 40) / frames) % frames;
+
+      ctx.drawImage(this.sprite, this.spriteCords.jumping[currentFrame][0], this.spriteCords.jumping[currentFrame][1], this.model.width, this.model.height, this.x, this.y, this.model.width, this.model.height);
+      return;
+    }
+
+    //Jumping left
+    if(state == this.spriteState.jumpLeft) {
+      frames = this.spriteCords.jumping.length;
+      currentFrame = Math.floor((elapsedTime / 40) / frames) % frames;
+
+      ctx.save()
+      ctx.translate(canvasWidth, 0);
+      ctx.scale(-1, 1);
+      ctx.drawImage(this.sprite, this.spriteCords.jumping[currentFrame][0], this.spriteCords.jumping[currentFrame][1], this.model.width, this.model.height, this.x - this.model.width, this.y, this.model.width, this.model.height);
+      ctx.restore();
+    }
+  };
+
+  this.spriteState = {
+    idleRight : 0,
+    idleLeft : 1,
+    walkingRight : 2,
+    walkingLeft : 3,
+    jumpRight : 4,
+    jumpLeft : 5
+  };
+
+  this.spriteCords = {
+    walking: [[6,88],[62,88],[116,88],[172,88]],
+    idle: [[8, 2]],
+    jumping: [[228, 88]]
   };
 }
